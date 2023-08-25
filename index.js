d8818df1dbcc9efad48a7cdb8398e1b98e3cc6b5
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-08-25 15:09:49 -0600

    Outline de boton en funcion articulo

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ var sumaTotal = 0;
 function agregarArticulo(){
     let nombreArticulo = document.getElementById("articulotxt").value
     let precioArticulo = document.getElementById("preciotxt").value
+    sumaTotal += parseInt(precioArticulo)
+    agregarTicket(nombreArticulo, precioArticulo);
+    actualizarTotal();
 }
