b2d0a60cb034c387bf55711c71ba770d2b584fe5
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-08-25 15:07:32 -0600

    Agregada funcion agregarArticulo

---
+++ index.js
@@ -0,0 +1,6 @@
+var sumaTotal = 0;
+
+function agregarArticulo(){
+    let nombreArticulo = document.getElementById("articulotxt").value
+    let precioArticulo = document.getElementById("preciotxt").value
+}
